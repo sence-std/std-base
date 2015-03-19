fdadde6fa581b46a7e8d02aa6f773311e6ab73d4
liushengqi <liushengqi@lvye.com>
2015-03-19 18:47:52 +0800

    算法排序

---
+++ std-algorithm/src/main/java/com/std/sort/BubbleSort.java
@@ -0,0 +1,38 @@
+/**
+ * @FileName:BubbleSort
+ * @Package: com.std.sort
+ *
+ * @author sence
+ * @created 3/19/2015 6:04 PM
+ *
+ * Copyright 2011-2015 Asura
+ */
+package com.std.sort;
+
+/**
+ *
+ * <p>冒泡排序算法</p>
+ *
+ * <PRE>
+ * <BR>	修改记录
+ * <BR>-----------------------------------------------
+ * <BR>	修改日期			修改人			修改内容
+ * </PRE>
+ *
+ * @author sence
+ * @since 1.0
+ * @version 1.0
+ */
+public class BubbleSort<T extends Integer> {
+
+	/**
+	 * 排序
+	 * @param t
+	 * @return
+	 */
+	public T[] doSort(T[] t){
+		return t;
+	}
+
+
+}
