270ef2eaa15f9836eb18696a696efd415f96f7a9
liushengqi <liushengqi@lvye.com>
2015-03-20 14:32:16 +0800

    希尔排序

--- std-algorithm/src/main/java/com/std/sort/ShellSort.java
+++ std-algorithm/src/main/java/com/std/sort/ShellSort.java
@@ -9,6 +9,8 @@
  */
 package com.std.sort;
 
+import com.std.util.ArrayUtil;
+
 /**
  *
  * <p>希尔排序</p>
@@ -25,6 +27,27 @@ package com.std.sort;
  */
 public class ShellSort {
 
+	public void shellSort(int[] arr){
+		int inner,outer;
+		int temp;
 
+		int h=1;
+		while(h<arr.length/3){
+			h=h*3+1;
+		}
+		while(h>0){
+			for(outer=h;outer<arr.length;outer++){
+				temp = arr[outer];
+				inner = outer;
+				while (inner>h-1&&arr[inner-h]>=temp){
+					arr[inner] = arr[inner-h];
+					inner -= h;
+				}
+				arr[inner] = temp;
+				ArrayUtil.printIntArray(arr, ",");
+			}
+			h=(h-1)/3;
+		}
+	}
 
 }
--- std-algorithm/src/test/java/com/std/sort/ShellSortTest.java
+++ std-algorithm/src/test/java/com/std/sort/ShellSortTest.java
@@ -1,17 +1,19 @@
 /**
- * @FileName:MergeSort
+ * @FileName:ShellSortTest
  * @Package: com.std.sort
  *
  * @author sence
- * @created 3/20/2015 11:39 AM
+ * @created 3/20/2015 2:26 PM
  *
  * Copyright 2011-2015 Asura
  */
 package com.std.sort;
 
+import org.junit.Test;
+
 /**
  *
- * <p>归并排序</p>
+ * <p>希尔排序</p>
  *
  * <PRE>
  * <BR>	修改记录
@@ -23,9 +25,17 @@ package com.std.sort;
  * @since 1.0
  * @version 1.0
  */
-public class MergeSort {
+public class ShellSortTest {
 
-
+	/**
+	 *
+	 */
+	@Test
+	public void test(){
+		int[] array = {3,4,2,1,8,5,9,7,10,6};
+		ShellSort shellSort = new ShellSort();
+		shellSort.shellSort(array);
+	}
 
 
 }
