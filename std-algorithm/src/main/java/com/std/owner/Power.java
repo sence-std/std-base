a58340cad1e7cca33a634bed2376729c3e448db6
liushengqi <liushengqi@lvye.com>
2015-03-26 10:39:03 +0800

    乘方运算的 完善

--- std-algorithm/src/main/java/com/std/owner/Power.java
+++ std-algorithm/src/main/java/com/std/owner/Power.java
@@ -11,23 +11,46 @@ public class Power {
 
     /**
      * 乘方 使用乘法 递归
+	 * 1 确保代码的鲁棒性
+	 * 2 确保可扩展性
      * @param x
      * @param y
      * @return
      */
-    public int doPower(int x,int y){
+    public double doPower(double x,int y){
+		if(x == 0){
+			return 0;
+		}
+		if(x==1){
+			return 1;
+		}
+		int _y = y;
+		if(y<0){
+			_y = Math.abs(y);
+		}
+		double result = calPower(x,_y);
+		if(y<0){
+			return 1/result;
+		}else{
+			return result;
+		}
+
+    }
+
+	public double calPower(double x,int y){
 		if(y==0){
 			return 1;
 		}
-        if(y==1){
-            return x;
-        }
-        if((y & 0x1)==1){
-            return x*doPower(x*x, y >> 2);
-        }else {
-            return doPower(x*x, y >> 2);
-        }
-    }
+		if(y==1){
+			return x;
+		}
+		if((y & 0x1)==1){
+			return x * calPower(x * x, y >> 1);
+		}else {
+			return calPower(x*x,y >> 1);
+		}
+
+	}
 
     public int doPower2(int x,int y){
         if(y==1){
@@ -38,7 +61,6 @@ public class Power {
 
     public static void main(String[] args) {
         Power p = new Power();
-        System.out.println(p.doPower(3, 18));
-        System.out.println(p.doPower2(3, 18));
+        System.out.println(p.doPower(2, -3));
     }
 }
