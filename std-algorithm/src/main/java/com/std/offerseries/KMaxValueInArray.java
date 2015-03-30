a1a5a05a0ec1ceb7c084ca7a1a6bbb7eda230768
liushengqi <liushengqi@lvye.com>
2015-03-30 15:47:58 +0800

    查找数组中第K大的数字

---
+++ std-algorithm/src/main/java/com/std/offerseries/KMaxValueInArray.java
@@ -0,0 +1,34 @@
+/**
+ * @FileName:KMaxValueInArray
+ * @Package: com.std.offerseries
+ *
+ * @author sence
+ * @created 3/30/2015 3:45 PM
+ *
+ * Copyright 2011-2015 Asura
+ */
+package com.std.offerseries;
+
+/**
+ *
+ * <p>
+ *     O(n)数组中第K大的数字
+ *     快速排序
+ * </p>
+ *
+ * <PRE>
+ * <BR>	修改记录
+ * <BR>-----------------------------------------------
+ * <BR>	修改日期			修改人			修改内容
+ * </PRE>
+ *
+ * @author sence
+ * @since 1.0
+ * @version 1.0
+ */
+public class KMaxValueInArray {
+
+
+
+
+}
