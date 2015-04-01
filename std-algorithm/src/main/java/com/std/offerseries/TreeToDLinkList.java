dfc4e66f96870d2dbb36e06945fd05b74ec02cde
liushengqi <liushengqi@lvye.com>
2015-04-01 14:52:25 +0800

    树转换成二叉树算法

---
+++ std-algorithm/src/main/java/com/std/offerseries/TreeToDLinkList.java
@@ -0,0 +1,37 @@
+/**
+ * @FileName:TreeToDLinkList
+ * @Package: com.std.offerseries
+ *
+ * @author sence
+ * @created 3/31/2015 5:52 PM
+ *
+ * Copyright 2011-2015 Asura
+ */
+package com.std.offerseries;
+
+/**
+ *
+ * <p>二叉树转换成双向链表</p>
+ *
+ * <PRE>
+ * <BR>	修改记录
+ * <BR>-----------------------------------------------
+ * <BR>	修改日期			修改人			修改内容
+ * </PRE>
+ *
+ * @author sence
+ * @since 1.0
+ * @version 1.0
+ */
+public class TreeToDLinkList {
+
+	/**
+	 * 二叉树转换成双向链表
+	 * @return
+	 */
+//	public BinaryTree toDLinkList(){
+//
+//	}
+
+
+}
--- std-algorithm/src/test/java/com/std/datastructure/TreeTest.java
+++ std-algorithm/src/test/java/com/std/datastructure/TreeTest.java
@@ -48,4 +48,5 @@ public class TreeTest {
 		System.out.println();
 	}
 
+
 }
